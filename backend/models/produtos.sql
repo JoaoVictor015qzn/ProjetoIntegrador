19235ac69c6c06cfe9bc6097c1b5426d2e6d6d53
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-10-29 11:12:35 -0300

    chore: tabela de produtos criada, e pronta para ser adcionada no banco

---
+++ backend/models/produtos.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Produtos (
+    id SERIAL PRIMARY KEY,
+    categoria_id INTEGER REFERENCES Categoria(id),
+    nome_produto VARCHAR(100) NOT NULL,
+    preco NUMERIC(10,2) NOT NULL,
+    status BOOLEAN DEFAULT TRUE,
+    imagem_principal TEXT,
+    quantidade_estoque INT DEFAULT 0,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
