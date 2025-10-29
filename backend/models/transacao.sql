50a2b47eea56438307e24b82926e0b160c405204
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-10-29 11:52:26 -0300

    chore: tabela de transacao criada, e pronta para ser adcionada no banco

---
+++ backend/models/transacao.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Transacao (
+    id SERIAL PRIMARY KEY,
+    pedidos_id INTEGER REFERENCES Pedidos(id),
+    metodo_pagamento VARCHAR(50),
+    valor_pago NUMERIC(10,2),
+    parcelas INT,
+    data_pagamento TIMESTAMP,
+);
