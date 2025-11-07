afe41b5afcd222828d34301b60d8dfba3d0f70bd
JujubsPaes <jujubspaes547@gmail.com>
2025-11-07 20:21:59 -0300

    Tela de Cadastro e Configurações

--- frontend/walk-app/src/App.tsx
+++ frontend/walk-app/src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
+import Cadastro from "./components/Cadastro/Cadastro";
+import CNPJ from "./components/CNPJ/Cnpj";
+import Config from "./components/Config/Config";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cnpj" element={<CNPJ />} />
+        <Route path="/config" element={<Config />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
